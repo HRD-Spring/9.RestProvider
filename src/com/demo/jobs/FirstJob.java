6aefb5b634af4e7d28674cd68afa88256912be3c
yinkokpheng <yinkokpheng@yahoo.com>
2016-04-03 20:09:21 +0700

    a

---
+++ src/com/demo/jobs/FirstJob.java
@@ -0,0 +1,16 @@
+package com.demo.jobs;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.StatefulJob;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class FirstJob extends QuartzJobBean implements StatefulJob {
+
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+		// TODO Auto-generated method stub
+		System.out.println("My first job has been executed....");
+	}
+
+}
