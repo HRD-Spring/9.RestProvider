d9aef4f4e09ec2c1d695fe1c9224ab32808fa28d
yinkokpheng <yinkokpheng@yahoo.com>
2016-04-04 01:31:57 +0700

    a

--- src/com/demo/pojo/User.java
+++ src/com/demo/pojo/User.java
@@ -35,6 +35,16 @@ public class User {
 	@Column(name="image")
 	private String image;
 	
+	@Column(name="role")
+	private String role;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
